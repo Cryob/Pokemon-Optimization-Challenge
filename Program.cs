24fb6462353c8482ca798314ce611bfd50533a9e
Cryob <tjbrady0821@gmail.com>
2024-08-19 20:40:10 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Random rand = new Random();
+int[] rolledNumbers = new int[4];
+
+int maxSims = 1000000000;
+int currentSim = 0;
+int highestValue = 0;
+
+while (currentSim < maxSims)
+{
+    rolledNumbers = new int[4];
+
+    for (int i = 0; i < 231; i++)
+    {
+        rolledNumbers[rand.Next(0, 4)] += 1;
+    }
+
+    if (highestValue < rolledNumbers[0])
+    {
+        highestValue = rolledNumbers[0];
+    }
+
+    currentSim++;
+}
+
+Console.WriteLine("Highest Value : " + highestValue);
+Console.WriteLine("Simulations : " + maxSims);
